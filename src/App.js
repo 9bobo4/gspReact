887af16cbd17e2214033c91341535cd05f66e612
9bobo4 <slobotadic76@gmail.com>
2022-06-14 19:50:26 +0200

    kreirana pocetna stranica

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@
 import './App.css';
 import Footer from './komponente/Footer';
 import NavBar from './komponente/NavBar';
+import Pocetna from './komponente/Pocetna';
 
 function App() {
   return (
     <div >
         <NavBar></NavBar>
+        <Pocetna></Pocetna>
         <Footer></Footer>
     </div>
   );
