6b48e6cc23ed737d5d8d6984766e8b6550ccef0f
9bobo4 <slobotadic76@gmail.com>
2022-06-14 21:37:58 +0200

    omoguceno pretrazivanje linija po pocetnoj ili krajnoj stanici

--- src/App.js
+++ src/App.js
@@ -80,8 +80,6 @@ function izbaciIzOmiljenih(id){
 
 
 
-
-
   return (
 
 
--- src/komponente/Linije.jsx
+++ src/komponente/Linije.jsx
@@ -1,13 +1,26 @@
 import React from 'react'
 import LinijeKartica from './LinijeKartica';
- 
+import {BsSearch} from "react-icons/bs"
+import { useState } from "react";
 function Linije({linije,dodajOmiljenu}) {
-
+    const [searchTerm, setSearchTerm] = useState('');
+    function handleInput(e){
+        setSearchTerm(e.target.value.toLowerCase() );
+        console.log(e.target.value);
+      }
+      
+      
   return (
+    
     <div className='sveLinije'>
-      {linije.map((l)=>(<LinijeKartica key={l.id} linija={l} dodajOmiljenu={dodajOmiljenu} mod={1} ></LinijeKartica>))}
-
-       
+    <input type="text" placeholder='Pretraga...' onChange={handleInput}></input>  <BsSearch />
+    {searchTerm=='' ? 
+        linije.map((l)=>(<LinijeKartica key={l.id} linija={l} dodajOmiljenu={dodajOmiljenu} mod={1} ></LinijeKartica>))
+        :
+        linije
+        .filter((l)=> (l.pocetnaStanica.toLowerCase().includes(searchTerm) || l.krajnjaStanica.toLowerCase().includes(searchTerm)) )
+        .map((l)=>(<LinijeKartica key={l.id} linija={l} dodajOmiljenu={dodajOmiljenu} mod={1} ></LinijeKartica>))
+    }
     </div>
   );
 }
